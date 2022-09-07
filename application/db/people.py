1b1f953d9ecf5a555203027843425eb62b3eacd5
XiXnice <kuzminov399@yandex.ru>
2022-09-07 22:37:30 +0300

    first commit

---
+++ application/db/people.py
@@ -0,0 +1,5 @@
+def get_employees():
+    a = int(input('Введите 1 число: '))
+    b = int(input('Введите 2 число: '))
+    c = a + b
+    print(c)
---
+++ application/salary.py
@@ -0,0 +1,5 @@
+def calculate_salary():
+    a = str(input('Введите 1 букву: '))
+    b = str(input('Введите 2 букву: '))
+    c = a + b
+    print(c)
